02ecc34d1cb2ce6b9d5b4a31ede0863099349ffb
SophieToescher <80051469+SophieToescher@users.noreply.github.com>
2021-05-17 15:00:49 +0200

    Add files via upload

---
+++ Toescher_Wetter.py
@@ -0,0 +1,16 @@
+# einfaches'='Zeichen: Zuweisung
+
+strWetter = input("Wochenendwetter - 'sonnig' oder 'regnerisch'")
+
+# doppeltes'=='Zeichen: Vergleichsoperator
+
+if strWetter == "sonnig":
+    print("Gartenparty")
+    print("Wuhuuuu")
+
+elif strWetter == "regnerisch":
+    print("Kellerparty")
+    print("Mist")
+
+else:
+    print("bitte entweder 'sonnig! oder 'regnerisch' eingeben!")
